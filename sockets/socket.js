f436e0b507071d447f5d53b462a5c0b8fea2673e
Manuel Valle <mvp21121@gmail.com>
2022-07-31 20:07:10 -0600

    Fin sección 3

---
+++ sockets/socket.js
@@ -0,0 +1,15 @@
+const { io } = require('../index');
+
+//mensajes io de socket.io
+io.on("connection", client => { 
+    console.log('Cliente conectado :D'); 
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado :(');
+     });
+
+     client.on('mensaje', ( payload ) => {
+        console.log('mensaje', payload);
+
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje '} );
+     });
+ });
